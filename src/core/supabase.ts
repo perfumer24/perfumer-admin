87830c9b684db5fcf0c615ae39139ecf0cb2d7bf
matt <wootaejung24@gmail.com>
2023-12-14 14:57:02 +0900

    client 타입 선언 후 적용

--- src/core/supabase.ts
+++ src/core/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@/config";
 import { Database } from "@/types/database";
 
-let client: ReturnType<typeof createClient<Database>> | undefined;
+export type Client = ReturnType<typeof createClient<Database>> | undefined;
 
 export function getSupabaseBrowserClient() {
   if (client) {
--- src/queries/getBrands.ts
+++ src/queries/getBrands.ts
@@ -1,3 +1,5 @@
-export function getBrands(client) {
-  return client.from("brand").select("*");
+import { Client } from "@/core/supabase";
+
+export function getBrands(client: Client) {
+  return client.from("brand").select("*").throwOnError("Could not get brands");
 }
