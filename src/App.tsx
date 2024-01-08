f420102fa75d224facabbedeb78afe6e5e88afb9
matt <wootaejung24@gmail.com>
2024-01-08 15:59:00 +0900

    로그인 유무에 따라 보여줄 페이지 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import AuthPage from "./pages/AuthPage";
+import useSupabase from "./hooks/useSupabase";
 
 export default function App() {
-  // 로그인 여부에 따라 login vs Outlet
-  const isLoggedIn = false;
-  if (isLoggedIn)
+  const [session, setSession] = useState(null);
+  const supabase = useSupabase();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log("session", session);
+      setSession(session);
+    });
+  }, [supabase]);
+
+  if (session)
     return (
       <>
         <NavBar />
