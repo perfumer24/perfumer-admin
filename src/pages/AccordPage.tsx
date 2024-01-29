a4f855bf73aafc69ccf6607ecdc5fb3ba2d56548
matt <wootaejung24@gmail.com>
2024-01-29 16:45:07 +0900

    향조 리스트 페이지 Markup

--- src/pages/AccordPage.tsx
+++ src/pages/AccordPage.tsx
@@ -3,9 +3,66 @@ import React from "react";
 
 export default function AccordPage() {
   return (
-    <>
+    <div>
       <NavBar />
-      accord향조
-    </>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>No.</th>
+            <th>유형</th>
+            <th>향조</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="">
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>1</td>
+            <td>
+              <div>로즈</div>
+            </td>
+            <td>
+              <div>로즈(장미)</div>
+            </td>
+          </tr>
+          <tr className="">
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>1</td>
+            <td>
+              <div>로즈</div>
+            </td>
+            <td>
+              <div>로즈(장미)</div>
+            </td>
+          </tr>
+          <tr className="">
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>1</td>
+            <td>
+              <div>로즈</div>
+            </td>
+            <td>
+              <div>로즈(장미)</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
