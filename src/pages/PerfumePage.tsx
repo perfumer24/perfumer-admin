1156e3e5bbdbb94d48c1c367f2c48b53feaa161f
matt <wootaejung24@gmail.com>
2024-01-29 15:59:06 +0900

    향수 table Markup with 더미데이터

--- src/pages/PerfumePage.tsx
+++ src/pages/PerfumePage.tsx
@@ -1,5 +1,226 @@
 import React from "react";
 
 export default function PerfumePage() {
-  return <>perfume</>;
+  return (
+    <div>
+      <div class="overflow-x-auto">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>No.</th>
+              <th>브랜드</th>
+              <th>이름</th>
+              <th>유형</th>
+              <th>향조</th>
+              <th>가격대</th>
+              <th>판매링크</th>
+              <th>출시년도</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>1</td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>유쏘풀</div>
+                </div>
+              </td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>히페리온</div>
+                </div>
+              </td>
+              <td>로즈, 우디</td>
+              <td>로즈(장미),시더우드(삼나무),베티버,머스크</td>
+              <td>15ml 36,000 50ml 58,000</td>
+              <td>바로가기</td>
+              <td>2022</td>
+            </tr>
+            <tr class="">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>1</td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>유쏘풀</div>
+                </div>
+              </td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>히페리온</div>
+                </div>
+              </td>
+              <td>로즈, 우디</td>
+              <td>로즈(장미),시더우드(삼나무),베티버,머스크</td>
+              <td>15ml 36,000 50ml 58,000</td>
+              <td>바로가기</td>
+              <td>2022</td>
+            </tr>
+            <tr class="">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>1</td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>유쏘풀</div>
+                </div>
+              </td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>히페리온</div>
+                </div>
+              </td>
+              <td>로즈, 우디</td>
+              <td>로즈(장미),시더우드(삼나무),베티버,머스크</td>
+              <td>15ml 36,000 50ml 58,000</td>
+              <td>바로가기</td>
+              <td>2022</td>
+            </tr>
+            <tr class="">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>1</td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>유쏘풀</div>
+                </div>
+              </td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>히페리온</div>
+                </div>
+              </td>
+              <td>로즈, 우디</td>
+              <td>로즈(장미),시더우드(삼나무),베티버,머스크</td>
+              <td>15ml 36,000 50ml 58,000</td>
+              <td>바로가기</td>
+              <td>2022</td>
+            </tr>
+            <tr class="bg-base-56">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>1</td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>유쏘풀</div>
+                </div>
+              </td>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://picsum.photos/56"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>히페리온</div>
+                </div>
+              </td>
+              <td>로즈, 우디</td>
+              <td>로즈(장미),시더우드(삼나무),베티버,머스크</td>
+              <td>15ml 36,000 50ml 58,000</td>
+              <td>바로가기</td>
+              <td>2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
